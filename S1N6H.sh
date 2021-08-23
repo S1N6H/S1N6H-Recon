9ac795c3a103818645ab2ff9f4e56b75d239c207
Harinder Singh <89349529+S1N6H@users.noreply.github.com>
2021-08-23 09:25:32 +0530

    Update S1N6H.sh

--- S1N6H.sh
+++ S1N6H.sh
@@ -7,7 +7,7 @@ import sys
 
 
 domain = sys.argv[1]
-commands = [ 'findomain -t '+domain+' -o;subfinder -d '+domain+' -o '+domain+'_subfinder.txt ;assetfinder --subs-only '+domain+' >> '+domain+'_assetfinder.txt; amass enum -d '+domain+' -o '+domain+'_amass.txt ; python /home/kali/subbrute/subbrute.py '+domain+' -o '+domain+'_subbrute.txt ; amass enum --passive -d '+domain+' -o '+domain+'_amass_passive.txt;amass enum  -src -ip -brute -min-for-recursive 2 -d '+domain+' -o '+domain+'_amass_passive.txt; gau -subs '+domain+' | cut -d / -f 3 | sort -u - '+domain+'_gau.txt; sublist3r -d '+domain+' -o '+domain+'_sublist3r.txt ;cat *.txt | sort -u >> '+domain+'_final_domains.txt ;cat '+domain+'_final_domains.txt | sort -u >> valid_subs.txt;']
+commands = [ 'findomain -t '+domain+' -o;subfinder -d '+domain+' -o '+domain+'_subfinder.txt ;assetfinder --subs-only '+domain+' >> '+domain+'_assetfinder.txt; amass enum -d '+domain+' -o '+domain+'_amass.txt ; python /home/kali/subbrute/subbrute.py '+domain+' -o '+domain+'_subbrute.txt ; amass enum --passive -d '+domain+' -o '+domain+'_amass_passive.txt;amass enum  -src -ip -brute -min-for-recursive 2 -d '+domain+' -o '+domain+'_amass_passive.txt; gau -subs '+domain+' | cut -d / -f 3 | sort -u - '+domain+'_gau.txt; sublist3r -d '+domain+' -o '+domain+'_sublist3r.txt ;cat *.txt | sort -u >> '+domain+'_final_domains.txt ;cat '+domain+'_final_domains.txt | httpx | sort -u >> valid_subs.txt;']
 count = 0  
 processes = []
 for com in commands:
